687b9e8d9db949481fa2e1a9f6d67c30b2814b81
TarasNovik <148384148+TarasNovik@users.noreply.github.com>
2024-07-16 15:57:36 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,7 @@
 `use strict`;
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = (quantity * pricePerDroid);
-    const message = (`You ordered  ${quantity} droids worth ${totalPrice} credits!`
-    );
+    const message = (`You ordered  ${quantity} droids worth ${totalPrice} credits!`);
     return (message);
 }
 
